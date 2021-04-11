92077ab9417d1952a3a4438210f275402c2b8e4a
gary-mcmonagle <garybmcmonagle@gmail.com>
2021-04-11 15:57:19 +0100

    running

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import { Selector } from 'testcafe';
+const moment = require('moment');
 
 
 fixture `Getting Started`
 .page `https://bookwkg.freedom-leisure.co.uk/withdeanbookings/Account/LogOn`;
 
-
+const getDate = () => {
+    const threeDaysTime = moment().add(2, 'days');
+    return threeDaysTime.format("DD/MM/YYYY")
+}
 
 const loginNow = async(t) => {
     await t
-    .typeText('#UserName', '*******')
-    .typeText('#Password', '*******')
+    .typeText('#UserName', '******')
+    .typeText('#Password', '******')
     .click('#LogOn > div > form > div > fieldset > p:nth-child(5) > input[type=submit]');
 }
 const selectGymClasses = async(t) => {
@@ -23,13 +27,28 @@ const selectGymClasses = async(t) => {
 const getGyms = async(t) => {
     await selectGymClasses(t)
     await t
-    .typeText('#SearchDate', '21/4/2021')
+    .click('#SearchDate')
+    .pressKey('ctrl+a delete');
+    await t
+    .typeText('#SearchDate', getDate())
     .click('#SearchButtonDiv > input');
+    await t
+    .click('#SearchButtonDiv > input');
+
 }
 
+const addEarliestSession = async (t) => {
+    await t.click('#basketControl_78_1')
+} 
 
 const getClasses = async(t) => {
     await t.click('#searchForClass')
+}
+
+const book =  async (t) => {
+    await t.click('#TermsAccepted');
+    await t.click('#CheckoutSubmit');
+    await t.click('#CentralRegion > div.main-content > div > div > p > a');
 }
 
 const logIn = async () => {
@@ -37,8 +56,9 @@ const logIn = async () => {
         await loginNow(t);
         await getClasses(t);
         await getGyms(t);
+        await addEarliestSession(t);
+        await book(t);
     });
-
 }
 
 const run = async () => {
